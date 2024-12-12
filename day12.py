7475bcff7ba986f6824c140cf2d79d0b8a17412f
hugues_esc <hugues.escoffier@gmail.com>
2024-12-12 17:04:16 +0100

    I quit this challenge to preserve my mental health

--- day12.py
+++ day12.py
@@ -1,5 +1,7 @@
 # Day12 - AoC 2024 
 
+# Many thanks to Léonie for the help on this one 
+
 
 def load_data(file):
     with open(file, "r") as file:
@@ -83,11 +85,6 @@ def is_connection(data, position, actual_letter):
     return all_connections, actual_letter
 
 
-def is_fence_here(all_fences, position):
-    # part2 ? 
-    pass 
-
-
 def perimeter_from_region(data, region):
     fences = 0
     actual_letter = region[0]
@@ -153,26 +150,69 @@ def check_right(data, pos, actual_letter):
     return False, pos, actual_letter
 
 
+def process_all_possible_corners(data):
+    all_possible_corners = []
+    for y in range(len(data)): 
+        for x in range(len(data[0])): 
+            corners = corners_of_position((y, x))
+            all_possible_corners.extend(corners)
+    return all_possible_corners
+
 def perimeter_from_region_advanced(data, region):
+    all_possible_corners = process_all_possible_corners(data)
     line_of_fences = 0
+    corner_that_i_hate = []
     lines = [] # [position, direction]
-    actual_letter = region[0]
+    all_corners = []
+    remaining_corners = []
     for pos in region[1]: 
-        nb_neighbors = 4
-        is_ok, _, actual_letter = check_above(data, pos, actual_letter)
-        if not is_ok:
-            nb_neighbors -= 1
-        is_ok, _, actual_letter = check_below(data, pos, actual_letter)
-        if not is_ok:
-            nb_neighbors -= 1
-        is_ok, _, actual_letter = check_left(data, pos, actual_letter)
-        if not is_ok:
-            nb_neighbors -= 1
-        is_ok, _, actual_letter = check_right(data, pos, actual_letter)
-        if not is_ok:
-            not nb_neighbors -= 1
-        fences += nb_neighbors
-    return fences
+        corners = corners_of_position(pos)
+        all_corners.extend(corners)
+    for corner in all_corners:
+        # print(corner)
+        # print(all_corners.count(corner))
+        if all_corners.count(corner) % 2 == 1:
+            remaining_corners.append(corner)
+        elif all_corners.count(corner) == 2:
+            # Check if it the corner in the center of a square
+            square_corners = calcul_the_square(corner)
+            # print(square_corners[0])
+            # print(square_corners[3])
+            # print()
+            if (square_corners[0] in all_corners) and (square_corners[3] in all_corners):
+                corner_that_i_hate.append(corner)
+            elif (square_corners[1] in all_corners) and (square_corners[2] in all_corners): 
+            # elif (square_corners[1] and square_corners[2]) in all_corners:
+                corner_that_i_hate.append(corner)
+                # remaining_corners.append(corner) 
+    line_of_fences = len(list(set(remaining_corners)))
+    # final = list(set(all_possible_corners).intersection(set(corner_that_i_hate)))
+    # print(list(set(final)))
+    yeah = len(list(set(corner_that_i_hate))) // 9 
+    return line_of_fences, yeah
+
+
+def calcul_the_square(corner):
+    y, x = corner
+    corner1 =  (y - 1, x - 1)
+    corner2 =  (y + 1, x - 1)
+    corner3 =  (y - 1, x + 1)
+    corner4 =  (y + 1, x + 1)
+    square_corners = [corner1, corner2, corner3, corner4]
+    return square_corners
+
+
+
+def corners_of_position(position):
+    y, x = position
+    upper_left = (y - 0.5, x - 0.5)
+    upper_right = (y + 0.5, x - 0.5)
+    lower_left = (y - 0.5, x + 0.5)
+    lower_right = (y + 0.5, x + 0.5)
+    corners = [upper_left, upper_right, lower_left, lower_right]
+    return corners
+
+
 def run_part1():
     data = load_data('data/day12_input.txt')
     sum = 0
@@ -184,7 +224,23 @@ def run_part1():
     print(sum)
 
 
+def run_part2(): 
+    data = load_data('data/day12_input.txt')
+    sum = 0
+    regions = identify_regions(data)
+    for region in regions: 
+        area = len(region[1])
+        yeah = 0
+        # print(region[0])
+        line_of_fences, yeah = perimeter_from_region_advanced(data, region)
+        # print(line_of_fences)
+        yeah = yeah * 2 
+        sum += area * (line_of_fences + yeah)
+    print(sum)
+
+
 if __name__ == '__main__':
     print('start')
-    run_part1()
+    # run_part1()
+    run_part2()
     print('end')
